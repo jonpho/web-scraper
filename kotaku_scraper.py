46c8c3eb162d1709539d1193e170d10d1594a2c6
Jonathan Pho <jonpho@gmail.com>
2021-01-21 00:59:06 -0600

    Updated scraper and utilities

--- kotaku_scraper.py
+++ kotaku_scraper.py
@@ -1,12 +1,16 @@
+from utilities import Utilities
 from response import Request
 
 url = 'https://kotaku.com/c/news'
-
+article = []
 kotaku_page = Request(url)
 results = kotaku_page.page_find_all('article')
 
-article = []
-
+def kotaku_date_cleanup(pulled_date):
+    pulled_date_with_year = Utilities.add_year_to_current_date(pulled_date)
+    cleaned_date = Utilities.convert_date_format(pulled_date_with_year)
+    return cleaned_date
+    
 for result in results:
     current_article = result
     article_dict = {}
@@ -16,7 +20,8 @@ for result in results:
         print(current_article.find_all('div')[1].text)
         article_dict['title'] = current_article.h2.text
         article_dict['link'] = current_article.find_all('a')[1]["href"]
-        article_dict['date'] = current_article.find_all('div')[1].text
+        article_dict['date'] = kotaku_date_cleanup(current_article.find_all('div')[1].text)
+        
         article.append(article_dict)
     except AttributeError:
         print("No article info in article")
--- utilities.py
+++ utilities.py
@@ -2,12 +2,17 @@ from datetime import datetime
 
 class Utilities:
 
-    def add_year_to_current_date(self, old_date):
+    @staticmethod
+    def add_year_to_current_date(old_date):
         # This will take a given month and day date format and add the current year to it.
         # old_date - Currently this only handles either Month and Day without slashes or comma's. Month can be digit, 3 letter month or fully spelled.
         return f"{old_date} {datetime.today().year}"
 
-    def convert_date_format(self, date_to_convert):
+    @staticmethod
+    def convert_date_format(date_to_convert):
         # Convert date format that is Month and Day to Month, Day, Year.
         # date_to_convert - The date format that needs to be used currently is a 3 leter month with day, and year. No comma's or slashes.
-        return datetime.strptime(date_to_convert, '%b %d %Y').date()
+        try:
+            return datetime.strptime(date_to_convert, '%b %d %Y').date().isoformat()
+        except ValueError:
+            return datetime.today().date().isoformat()
